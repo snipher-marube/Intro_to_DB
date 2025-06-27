2f41c286fa9e51056446850d40583403854b8948
Snipher Marube <sniphermarube@gmail.com>
2025-06-27 08:41:29 +0300

    readme

--- task_5.sql
+++ task_5.sql
@@ -1,3 +1,3 @@
--- auto-generate the ID
-INSERT INTO Customers (customer_name, email, address)
-VALUES ('Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+-- Insert a single row into the customer table with specified ID
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
